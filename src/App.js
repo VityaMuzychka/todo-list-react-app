8500ddeecd9fb860b908aac8f627b3707bc0a4c6
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-18 22:31:25 +0200

    code is modified by adding a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
+import { Task } from './Task';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -22,12 +23,8 @@ function App() {
      </div>
      <div className='list'>
       {todoList.map(task => {
-        return (
-           <div>
-             <h1>{task.taskName}</h1>
-             <button onClick={() => deleteTask(task.id)}> X </button>
-           </div>)}
-      )}
+       return <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask} />
+      })}
      </div>
     </div>
   );
